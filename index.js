3ab5248c37893e0e4d5f8b266d83e2afc2fc54c6
Kalinina Oksana <oksana108@gmail.com>
2023-03-05 00:01:17 +0400

    add lodash

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, Hexlet!');
+    import _ from "lodash";
+
+    console.log('Hello, Hexlet!');
+
+    _.intersection([2, 1], [2, 3]);
+    _.capitalize("hello");
+    
+    console.log(_.last(['one', 'two']));
